b9d1cc9b16e8e93265ab1d737f4cb321ed7166f6
gabrielpereira1603 <gabriel_ap02@yahoo.com>
2023-07-05 14:20:52 -0300

    teste

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <link rel="icon" href="assets/img/fiveicon.png" type="image/png">
+    <title>Somos Dev's</title>
+</head>
+<body>
+    <?php
+        include("cabecario.php");
+    ?>
+
+    <section class="main">
+        <div class="title"></div>
+    </section>
+    
+</body>
+</html>
